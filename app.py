2e1b6c51dfd8a7be227d725c2f49b1faaf701841
abhi92046 <167603776+abhi92046@users.noreply.github.com>
2025-01-31 19:36:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def proxy():
-    return redirect("https://sites.google.com/view/copy-of-rotex-updates-for-test/home", code=302)
+    return redirect("https://sites.google.com/view/rotexupdates/home", code=302)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
